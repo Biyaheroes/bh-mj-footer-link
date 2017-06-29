8fa6b23b102b6ea6d5f6b71ba7fe905375309634
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-06-29 18:24:57 +0800

    Updated bh-mj-footer-link

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-require("graceful-fs").gracefulify(require("fs"));
+require( "graceful-fs" ).gracefulify( require( "fs" ) );
 
-const babel = require("gulp-babel");
-const changed = require("gulp-changed");
-const child = require("child_process");
-const debug = require("gulp-debug");
-const del = require("del");
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const rename = require("gulp-rename");
-const sourcemap = require("gulp-sourcemaps");
-const sass = require("gulp-sass");
-const uglify = require("gulp-uglify");
-const watch = require("gulp-watch");
-const vinylPath = require("vinyl-paths");
+const babel = require( "gulp-babel" );
+const changed = require( "gulp-changed" );
+const child = require( "child_process" );
+const debug = require( "gulp-debug" );
+const del = require( "del" );
+const gulp = require( "gulp" );
+const plumber = require( "gulp-plumber" );
+const rename = require( "gulp-rename" );
+const sourcemap = require( "gulp-sourcemaps" );
+const sass = require( "gulp-sass" );
+const uglify = require( "gulp-uglify" );
+const watch = require( "gulp-watch" );
+const vinylPath = require( "vinyl-paths" );
 
 const mode = process.env.NODE_ENV;
-console.log(`gulp mode: ${mode}`);
+console.log( `gulp mode: ${ mode }` );
 
-gulp.task("style", function styleTask() {
-	del.sync("detail.css");
+gulp.task( "style", function styleTask( ){
+	del.sync( "detail.css" );
 
 	let style = [
 		"*.scss",
@@ -27,21 +27,21 @@ gulp.task("style", function styleTask() {
 		"!.*"
 	];
 
-	if (mode === "production") {
-		style.push("!**/*test*.scss");
+	if( mode === "production" ){
+		style.push( "!**/*test*.scss" );
 	}
 
-	return gulp.src(style)
-		.pipe(plumber())
-		.pipe(changed("./", { "extension": ".css" }))
-		.pipe(debug({ "title": "File:" }))
-		.pipe(sourcemap.init())
-		.pipe(sass({ "outputStyle": "compressed" }).on("error", sass.logError))
-		.pipe(sourcemap.write("./"))
-		.pipe(gulp.dest("./"));
-});
+	return gulp.src( style )
+		.pipe( plumber( ) )
+		.pipe( changed( "./", { "extension": ".css" } ) )
+		.pipe( debug( { "title": "File:" } ) )
+		.pipe( sourcemap.init( ) )
+		.pipe( sass( { "outputStyle": "compressed" } ).on( "error", sass.logError ) )
+		.pipe( sourcemap.write( "./" ) )
+		.pipe( gulp.dest( "./" ) );
+} );
 
-gulp.task("script", function scriptTask() {
+gulp.task( "script", function scriptTask( ){
 	let script = [
 		"*.jsx",
 		"./*/*.jsx",
@@ -50,17 +50,17 @@ gulp.task("script", function scriptTask() {
 		"!.*"
 	];
 
-	if (mode === "production") {
-		script.push("!**/*test*.jsx");
+	if( mode === "production" ){
+		script.push( "!**/*test*.jsx" );
 	}
 
-	return gulp.src(script)
-		.pipe(plumber())
-		.pipe(changed("./", { "extension": ".js" }))
-		.pipe(debug({ "title": "File:" }))
-		.pipe(sourcemap.init())
-		.pipe(babel())
-		.pipe(uglify({
+	return gulp.src( script )
+		.pipe( plumber( ) )
+		.pipe( changed( "./", { "extension": ".js" } ) )
+		.pipe( debug( { "title": "File:" } ) )
+		.pipe( sourcemap.init( ) )
+		.pipe( babel( ) )
+		.pipe( uglify( {
 			"compress": {
 				"keep_fargs": true,
 				"keep_fnames": true,
@@ -68,32 +68,32 @@ gulp.task("script", function scriptTask() {
 			},
 			"sourceMap": true,
 			"mangle": false
-		}))
-		.pipe(rename({ "extname": ".js" }))
-		.pipe(sourcemap.write("./"))
-		.pipe(gulp.dest("./"));
-});
+		} ) )
+		.pipe( rename( { "extname": ".js" } ) )
+		.pipe( sourcemap.write( "./" ) )
+		.pipe( gulp.dest( "./" ) );
+} );
 
-gulp.task("default", ["style", "script"]);
+gulp.task( "default", [ "style", "script" ] );
 
-gulp.task("watch", function watchTask() {
+gulp.task( "watch", function watchTask( ){
 	let state = "idle";
 
-	let rebuild = () => {
-		if (state == "running") {
+	let rebuild = ( ) => {
+		if( state == "running" ){
 			return;
 		}
 
 		state = "running";
 
-		child.execSync("npm run test", { "cwd": process.cwd(), "stdio": [0, 1, 2] });
+		child.execSync( "npm run test", { "cwd": process.cwd( ), "stdio": [ 0, 1, 2 ] } );
 
 		state = "idle";
 	};
 
-	rebuild();
+	rebuild( );
 
-	return watch([
+	return watch( [
 		"package.json",
 		"bower.json",
 		"test.html",
@@ -104,11 +104,11 @@ gulp.task("watch", function watchTask() {
 		"!./node_modules/**",
 		"!./bower_components/**",
 		"!.*"
-	], { "readDelay": 1000 }, rebuild);
-});
+	], { "readDelay": 1000 }, rebuild );
+} );
 
-gulp.task("clean", function cleanTask() {
-	return gulp.src([
+gulp.task( "clean", function cleanTask( ){
+	return gulp.src( [
 		"*.js",
 		"*.css",
 		"./*/*.js",
@@ -119,8 +119,8 @@ gulp.task("clean", function cleanTask() {
 		"!./node_modules/**",
 		"!./bower_components/**",
 		"!.*"
-	])
-		.pipe(plumber())
-		.pipe(debug({ "title": "File:" }))
-		.pipe(vinylPath(del));
-}); 
+	] )
+	.pipe( plumber( ) )
+	.pipe( debug( { "title": "File:" } ) )
+	.pipe( vinylPath( del ) );
+} );
