cbda77afeb365e596d75be55b306511107bd2fc7
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-07 16:23:42 +0800

    Update bh-mj-footer-link.

---
+++ component.jsx
@@ -0,0 +1,14 @@
+ 
+"use strict"; 
+ 
+import React from "react"; 
+import ReactDOM from "react-dom"; 
+ 
+import FooterLink from "./footer-link.js"; 
+ 
+ReactDOM.render( 
+	<FooterLink 												
+		list="W3sidGl0bGUiOiJIZWxsbyIsInZhbHVlIjoid29ybGQifSx7InRpdGxlIjoiSGVsbG8iLCJ2YWx1ZSI6IndvcmxkIn0seyJ0aXRsZSI6IkxvcmVtIElwc3VtIiwidmFsdWUiOiJIZWxsbyBXb3JsZCJ9LHsidGl0bGUiOiJIb2xhIEFtaWdvIiwidmFsdWUiOiJIZWxsbyBXb3JsZCBZZWFoIn1d">
+	</FooterLink>,
+	document.getElementById( "root" ) 
+); 
--- test.jsx
+++
@@ -1,9 +0,0 @@
- 
-"use strict"; 
- 
-import React from "react"; 
-import ReactDOM from "react-dom"; 
- 
-import FooterLink from "./footer-link.js"; 
- 
-ReactDOM.render( <FooterLink></FooterLink> ); 
